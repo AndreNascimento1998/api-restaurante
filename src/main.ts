28d37c8c6ea0835f4d2d39653871cd610e126588
André Linux <andre.ncardoso@hotmail.com>
2023-06-26 18:39:26 -0300

    configurando Cors p/ consumir api/lanche

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
+
+  const corsOptions: CorsOptions = {
+    origin: true, 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  };
+  app.enableCors(corsOptions);
+
   await app.listen(3000);
 }
 bootstrap();
